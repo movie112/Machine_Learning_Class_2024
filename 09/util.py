42454dd96f4a3269f76bd148d1b0d2ad484150de
YEONGHWA KIM <56140795+movie112@users.noreply.github.com>
2024-06-11 21:26:44 +0900

    Add files via upload

---
+++ 09/util.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+
+def compute_gradient_descent_step1(x, y, label, theta, lr):
+    # ==================================================
+    # complete the code
+    # ==================================================
+
+    return theta, loss 
+
+
+def compute_gradient_descent_step2(x, y, label, theta, lr):
+    # ==================================================
+    # complete the code
+    # ==================================================
+
+    return theta, loss 
+
+
+def plot_data_label(x, y, label):
+    # ==================================================
+    # complete the code
+    # ==================================================
+
+    plt.show()
+
+
+def plot_classifier1(x, y, label, theta):
+    # ==================================================
+    # complete the code
+    # ==================================================
+
+    plt.show()
+
+
+def plot_classifier2(x, y, label, theta):
+    # ==================================================
+    # complete the code
+    # ==================================================
+
+    plt.show()
+    
